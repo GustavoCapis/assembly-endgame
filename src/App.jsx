15224a4690d438126a6ce68656c390e3b5aad9df
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-01-30 11:58:55 -0300

    feat: Add word display section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react"
-import Header from "./Header"
-import Status from "./Status"
-import Languages from "./Languages"
-
+import React from "react";
+import Header from "./Header";
+import Status from "./Status";
+import Languages from "./Languages";
 
 export default function AssemblyEndgame() {
-    return (
-        <main>
-            <Header/>
-            <Status/>
-            <Languages/>
-        </main>
-    )
+  //set state for the word element
+  const [currentWord, setCurrentWord] = React.useState("react");
+  const letters = [...currentWord];
+  const wordElements = letters.map((letter, index) => (
+    <span className="letter" key={index}>
+      {letter.toUpperCase()}
+    </span>
+  ));
+
+  return (
+    <main>
+      <Header />
+      <Status />
+      <Languages />
+      <section className="word">{wordElements}</section>
+    </main>
+  );
 }
