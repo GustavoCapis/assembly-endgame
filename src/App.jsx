fabe8a03047f53a7284c1b38f6df0b4144924d6d
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-01-30 14:06:14 -0300

    feat: Save the guessed letters.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState} from "react";
 import Header from "./Header";
 import Status from "./Status";
 import Languages from "./Languages";
@@ -6,7 +6,15 @@ import Keyboard from "./Keyboard";
 
 export default function AssemblyEndgame() {
   //Set state for the word element
-  const [currentWord, setCurrentWord] = React.useState("react");
+  const [currentWord, setCurrentWord] = useState("react");
+
+  //Set state for the guessed letters
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  function handleClick(letter) {
+    setGuessedLetters((prev) => [...prev, letter]);
+  }
+
   const letters = [...currentWord];
   const wordElements = letters.map((letter, index) => (
     <span className="letter" key={index}>
@@ -20,7 +28,7 @@ export default function AssemblyEndgame() {
       <Status />
       <Languages />
       <section className="word">{wordElements}</section>
-      <Keyboard/>
+      <Keyboard handleClick={handleClick} />
       <button className="new-game">New Game</button>
     </main>
   );
--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -1,9 +1,13 @@
-export default function Keyboard() {
+export default function Keyboard(props) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   return (
     <section className="keyboard">
       {[...alphabet].map((letter) => (
-        <button className="keyboard-btn" key={letter}>
+        <button
+          onClick={()=> props.handleClick(letter)}
+          className="keyboard-btn"
+          key={letter}
+        >
           {letter.toUpperCase()}
         </button>
       ))}
